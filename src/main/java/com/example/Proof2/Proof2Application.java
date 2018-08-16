e39d7c993277cafba49eebd46d51684ff1a8b9a0
MXYZPLTK <rebornnxd@gmail.com>
2018-08-16 19:50:21 -0400

    11

---
+++ src/main/java/com/example/Proof2/Proof2Application.java
@@ -0,0 +1,12 @@
+package com.example.Proof2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proof2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proof2Application.class, args);
+	}
+}
